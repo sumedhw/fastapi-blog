fb3dd87cfbe1bfe69d614d66c478b250e5be0546
Sumedh Waghmare <sumedhw71@gmail.com>
2026-02-15 00:35:35 +0530

    Pydantic schemas

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from fastapi.responses import JSONResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from starlette.exceptions import HTTPException as StarletteHTTPException
+from schemas import PostCreate, PostResponse
 
 app = FastAPI()
 
@@ -51,11 +52,31 @@ def post_page(request : Request, post_id : int):
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post no found")
 
 
-@app.get("/api/posts")
+@app.get("/api/posts", response_model=list[PostResponse])
 def get_posts():
     return posts
 
-@app.get("/api/posts/{post_id}")
+
+@app.post(
+        "/api/posts",
+        response_model=PostResponse,
+        status_code=status.HTTP_201_CREATED
+)
+def create_post(post: PostCreate):
+    new_id =  max(p["id"] for p in posts) + 1 if posts else 1
+    new_post = {
+        "id": new_id,
+        "author":post.author,
+        "title": post.title,
+        "content": post.content,
+        "date_posted": "April 23, 2025",
+    }
+    posts.append(new_post)
+
+    return new_post
+
+
+@app.get("/api/posts/{post_id}", response_model=PostResponse)
 def get_post(post_id : int):
     for post in posts:
         if post.get("id") == post_id:
